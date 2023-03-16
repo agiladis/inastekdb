687ab42d37039f04806cfca606c762b0d8af87aa
cencendi <85205431+cencencendi@users.noreply.github.com>
2023-03-16 11:10:11 +0700

    Add files via upload

---
+++ include/sidebar.php
@@ -0,0 +1,38 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="images/logo-inastek-resized.png" alt="">
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="index.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-home"></span><span class="mtext">Ini Dashboard</span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="form-basic.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-pencil"></span><span class="mtext">Ini buat isi form</span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-table"></span><span class="mtext">Ini tabel</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="datatable.php">Tabel Asli</a></li>
+							<li><a href="datatable2.php">Tabel Gak Asli</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="login.php" class="dropdown-toggle no-arrow">
+							<span class="fa"></span><span class="mtext">Login Page</span>
+						</a>
+					</li>
+
+				</ul>
+			</div>
+		</div>
+	</div>
