df2491f3fa7aaabcdde50612c68d92f485767379
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-03-30 14:24:27 +0700

    create logic handle pcb and loadcell depend on selcet value on product category

--- vendors/scripts/request-by-category.js
+++ vendors/scripts/request-by-category.js
@@ -1,16 +1,19 @@
 function selectCategory() {
     let idCategory = document.querySelector("#kategori-produk").value;
     let kodeBatch = document.querySelector("#batch-produksi").value;
-
-    console.log("idCategory :" . idCategory);
-    console.log("kodeBatch :" . kodeBatch);
     
+    console.log(idCategory);
+
     $.ajax({
         url: "include/query-id-category.php",
         method: "POST",
-        data: {id_kategori: idCategory, kode_batch: kodeBatch},
+        data: {
+            id_kategori: idCategory, 
+            kode_batch: kodeBatch,
+        },
         success: function(data) {
-            $('#debug').html(data);
+            // $('#kode-nomor-container').html(data);
+            $('#conditional-form').html(data);
         }
     })
 }
