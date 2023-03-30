957b41475e07a1510c1d9e93a8b081eef52f4ef5
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-03-30 10:24:27 +0700

    add new condition to reset kode nomor on create serial number

--- vendors/scripts/request-by-category.js
+++ vendors/scripts/request-by-category.js
@@ -1,10 +1,14 @@
 function selectCategory() {
-    let idCategory = document.querySelector("#kategori_produk").value;
+    let idCategory = document.querySelector("#kategori-produk").value;
+    let kodeBatch = document.querySelector("#batch-produksi").value;
+
+    console.log("idCategory :" . idCategory);
+    console.log("kodeBatch :" . kodeBatch);
     
     $.ajax({
         url: "include/query-id-category.php",
         method: "POST",
-        data: {id: idCategory},
+        data: {id_kategori: idCategory, kode_batch: kodeBatch},
         success: function(data) {
             $('#debug').html(data);
         }
